600059764dd04a57ca955440bdc1cbb05748e567
EMBKSM <k33827925@gmail.com>
2024-09-25 23:34:41 +0900

    modify

--- loud_loud_boy_device_code/loud_loud_boy_device_code.ino
+++ loud_loud_boy_device_code/loud_loud_boy_device_code.ino
@@ -12,8 +12,11 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 const char* ssid = "name";     // wifi 이름
 const char* password="password";  // wifi 패스워드
 const char* serverAddress = "http://255.255.0.0:8080/plant_m/connect"; // 서버주소
-int time=0;
+int pre_time=0;
 int Server_time;
+int Alarm_time[20];
+int cnt=0;
+
 
 void setup() {
   Serial.begin(115200);
@@ -31,8 +34,20 @@ void setup() {
   pinMode(D5, INPUT);
   pinMode(D6, INPUT);
   pinMode(D7, OUTPUT);
+  pinMode(D8, INPUT);
+  if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      WiFiClient client;
+      http.begin(client, serverAddress); 
+      if()
+
+      http.end();
+  }
 }
 
 void loop() {
-  time = (millis()/60000)%1440;
+  pre_time = Server_time + (millis()/60000)%1440;// 밀리초를 분으로 바꾸기
+  if(pre_time >= Alarm_time[cnt%20]){
+    digitalWrite(D7,HIGH);
+  }
 }
