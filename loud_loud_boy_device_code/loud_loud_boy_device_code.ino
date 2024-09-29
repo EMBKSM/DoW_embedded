5ca8a11dcde3ce51c190fb54843dcc3035b6f9a9
EMBKSM <k33827925@gmail.com>
2024-09-29 22:48:38 +0900

    code modify

--- loud_loud_boy_device_code/loud_loud_boy_device_code.ino
+++ loud_loud_boy_device_code/loud_loud_boy_device_code.ino
@@ -1,16 +1,15 @@
 #include <Wire.h>               
-#include <Adafruit_GFX.h>      
-#include <Adafruit_SSD1306.h>      
+#include <U8g2lib.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
+
 #define SCREEN_WIDTH 128            
 #define SCREEN_HEIGHT 64             
 
 #define OLED_RESET     4             
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 using namespace std;
 
@@ -28,18 +27,23 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", 32400, 60000);
 
 String jwt_token ="0";
 int Alarm_time[21];
-array<String, 21> title;
-int cnt_1 = 0;
-int cnt_2 = 0;
-int cnt = 0;
+String Alarm_time_for_screen[21];
+String title[21];
+uint8_t cnt_1 = 0;
+uint8_t cnt_2 = 0;
+uint8_t cnt_3 = 0;
+uint8_t cnt = 0;
+uint8_t buzzer_on = 0;
+uint8_t page = 0;
+
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 void setup() {
+  u8g2.begin();
   timeClient.begin();
 
   Serial.begin(115200);
-
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -81,13 +85,51 @@ void setup() {
 }
 
 void loop(){
+  u8g2.clearBuffer();
   timeClient.update();
+
+
+
   String currentTime = timeClient.getFormattedTime();
   int hours = currentTime.substring(0, 2).toInt();
   int minutes = currentTime.substring(3, 5).toInt()+ (hours*60);
+
+  
+  screen_print(page);
+  
+  server_receive();
+
+  if(minutes >= Alarm_time[0]){
+    digitalWrite(D7,HIGH);
+    buzzer_on = 1;
+  }
+  if(buzzer_on == 1){
+    if(digitalRead(D8) == HIGH){
+    digitalWrite(D7,LOW);
+    }
+  }
+  u8g2.sendBuffer();
+
+}
+
+void screen_print(uint8_t n){
+  u8g2.setFont(u8g2_font_6x10_tf);  
+  u8g2.drawStr(0, 0, return_char_Array(Alarm_time_for_screen[n]));
+  u8g2.setFont(u8g2_font_unifont_t_korean1);  
+  u8g2.drawStr(0, 8, return_char_Array(title[n].substring(0, 8)));
+  u8g2.drawStr(0, 24, return_char_Array(title[n].substring(8,16)));
+  u8g2.drawStr(0, 40, return_char_Array(title[n].substring(16,24)));
+}
+
+void server_receive(){
   if (client.available()) {
-    if(cnt_1 == cnt_2){
-      cnt++;
+    if(cnt_1 == cnt_2&& cnt_2 == cnt_3){
+      if(cnt == 239){
+        cnt = 0;
+        cnt_1 = 0;
+        cnt_2 = 0;
+        cnt_3 = 0;
+      }
     }
     String line = client.readStringUntil('\n');
     if(jwt_token == "0"){
@@ -96,18 +138,33 @@ void loop(){
         jwt_token = eventData;
       }
     }
-    if (line.startsWith("title: ")) {
+    else if (line.startsWith("title: ")) {
       String eventData = line.substring(7); 
-      title.at(cnt%20) = eventData;
+      title[cnt%20] = eventData;
       cnt_1++;
     }
-    if (line.startsWith("date: ")) {
-      String eventData = line.substring(6); 
+    else if (line.startsWith("time_data: ")) {
+      String eventData = line.substring(11); 
       Alarm_time[cnt%20] = strtoul(eventData.c_str(), NULL, 10);
       cnt_2++;
     }
-  }// 밀리초를 분으로 바꾸기
-  if(minutes >= Alarm_time[0]){
-    digitalWrite(D7,HIGH);
+    else if (line.startsWith("date: ")) {
+      String eventData = line.substring(6); 
+      Alarm_time_for_screen[cnt%20] = eventData;
+      cnt_3++;
+    }
   }
+}
+
+char* return_char_Array(String str){
+  char* charArray = stringToCharArray(str);
+  return charArray;
+}
+
+
+char* stringToCharArray(String input) {
+  int length = input.length() + 1; // null 문자 포함
+  char* charArray = new char[length]; // 동적 메모리 할당
+  input.toCharArray(charArray, length);
+  return charArray;
 }
